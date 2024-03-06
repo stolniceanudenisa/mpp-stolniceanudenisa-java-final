5bfcac570e470ec72469453e5d8d9de3f94903fd
denisaelenastolniceanu <denisa.stolniceanu@stud.ubbcluj.ro>
2024-03-06 23:09:09 +0200

    first commit - branch sapt3_entitati_repo

---
+++ Java/src/main/java/org/example/model/Entity.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import java.io.Serializable;
+
+public class Entity<ID> implements Serializable {
+
+    private static final long serialVersionUID = 7331115341259248461L;
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+}

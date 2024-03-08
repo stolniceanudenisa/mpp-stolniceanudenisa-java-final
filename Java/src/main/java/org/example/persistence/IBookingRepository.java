0e615f57b44d4d42112b86bc3b6e4d49e4ac9f0a
denisaelenastolniceanu <denisa.stolniceanu@stud.ubbcluj.ro>
2024-03-08 15:19:18 +0200

    almost final changes

--- Java/src/main/java/org/example/persistence/IBookingRepository.java
+++ Java/src/main/java/org/example/persistence/IBookingRepository.java
@@ -3,7 +3,5 @@ package org.example.persistence;
 import org.example.model.Booking;
 
 public interface IBookingRepository extends IRepository<Long, Booking>{
-
     int getNumberOfBookingsForFlight(Long flightId);
-
 }
